df20beb4cae340188668dce493599e6ee6da8633
JoaoPedroAmaralSouto <jamaralsouto@gmail.com>
2026-05-11 10:01:26 -0300

    Versão inicial do site apenas com as configurações do spring boot e o front-end do site

---
+++ site/voz-ativa/src/main/java/com/example/voz_ativa/VozAtivaApplication.java
@@ -0,0 +1,13 @@
+package com.example.voz_ativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozAtivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VozAtivaApplication.class, args);
+	}
+
+}
---
+++ site/voz-ativa/src/test/java/com/example/voz_ativa/VozAtivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voz_ativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VozAtivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
